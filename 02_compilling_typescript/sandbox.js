d98482217f2d4903dc647c0907696c01664e58bd
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-10 23:09:07 +0200

    compiling typeScript

---
+++ 02_compilling_typescript/sandbox.js
@@ -0,0 +1,10 @@
+var character = "Black";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log("typeof inputs = ", typeof inputs);
+console.log("Collection of input :", inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+var numbers = [0, 1, 3, 4];
+var username = "John";
---
+++ 02_compilling_typescript/sandbox.ts
@@ -0,0 +1,15 @@
+const character = "Black";
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+console.log("typeof inputs = ", typeof inputs);
+
+console.log("Collection of input :", inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
+
+let numbers = [0, 1, 3, 4];
+
+let username = "John";
