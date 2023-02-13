f19f23261d01e33201309695e9258697ff2e7798
danielOuattara <daniel.ouattara@gmx.com>
2023-02-13 19:07:52 +0100

    update

--- 04-Array-and-Object/sandbox.js
+++ 04-Array-and-Object/sandbox.js
@@ -1,26 +1,40 @@
-// arrays
+/* Arrays
+============*/
 var names = ["elle", "lui", "moi"];
 names.push("vous"); // --> OK
-names[names.length] = "Ils";
-//names.push(5);    // --> error
-//name[0] = 4;      // --> error
-//names = "song"; // --> Error
+names[names.length] = "Ils"; // --> OK
+names.push(5); // --> error
+name[0] = 4; // --> error
+names = "song"; // --> Error
+//-----
+var countries = [];
+countries.push("Burkina Faso");
+countries.push("Mali");
+countries.push("Guinnee");
+//-----
 // mix array: more manageable
 var mixed = ["ken", 4, false];
 mixed.push(12); // OK
 mixed[3] = "anything"; // OK
-// objects
+//-----
+var mixedValue = [];
+mixedValue.push(true);
+mixedValue.push("Hello");
+mixedValue.push(34);
+/* Objects
+==============*/
 var person = {
     firstName: "Joe",
     lastName: "Doe",
     age: 37
 };
 person.age = 38; // --> OK
-//person.age = "hello"; // --> Error
-// person.skills = ['computing', 'parachut jump'] //--> Error: not there when initialized
-// Updating an object
+person.age = "hello"; // --> Error
+person.skills = ["computing", "parachut jump"]; //--> Error: not there when initialized
+// Updating object values
 person = {
     firstName: "Ana",
     lastName: "Doe",
-    age: 29
+    age: 29,
+    skills: ["piano", "dancing"]
 };
--- 04-Array-and-Object/sandbox.ts
+++ 04-Array-and-Object/sandbox.ts
@@ -3,18 +3,31 @@
 
 let names = ["elle", "lui", "moi"];
 names.push("vous"); // --> OK
-names[names.length] = "Ils";
+names[names.length] = "Ils"; // --> OK
 names.push(5); // --> error
 name[0] = 4; // --> error
-
 names = "song"; // --> Error
 
+//-----
+const countries: string[] = [];
+countries.push("Burkina Faso");
+countries.push("Mali");
+countries.push("Guinnee");
+
+//-----
 // mix array: more manageable
 
 let mixed = ["ken", 4, false];
 
 mixed.push(12); // OK
 mixed[3] = "anything"; // OK
+
+//-----
+
+const mixedValue: (string | number | boolean)[] = [];
+mixedValue.push(true);
+mixedValue.push("Hello");
+mixedValue.push(34);
 
 /* Objects 
 ==============*/
