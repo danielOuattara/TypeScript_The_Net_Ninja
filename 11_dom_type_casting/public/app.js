b660895d13a5ceefa8b3010c845f44ecd9d00ee9
danielOuattara <daniel.ouattara@gmx.com>
2023-02-21 21:11:05 +0100

    logic update

--- 11_dom_type_casting/public/app.js
+++ 11_dom_type_casting/public/app.js
@@ -1,41 +1,76 @@
 "use strict";
 /* ---------------------------------
-Typscript DOM & Type Casting*/
+TypeScript DOM & Type Casting*/
 //----------------------------------------------------------------
 var anchor = document.querySelector("a");
 console.log("anchor = ", anchor);
-//console.log(anchor.href) // Sure that anchor exists to be able to access its "href" prop ?? 
-/* TypeScript didn't parse the DOM, it cannot be sure
+// console.log(anchor.href); // Error
+/*
+Error const anchor: HTMLAnchorElement | null .
+'anchor' is possibly 'null'.ts(18047)
+
+TypeScript didn't parse the DOM, it cannot be sure
 that "anchor" element exists in the DOM */
-// Option - 1
-if (anchor) { // option-1
-    // do something
-}
-else {
-    var anchor2 = document.querySelector("a"); // exclamation mark
+//------------------
+// Solution - 1
+var anchor2 = document.querySelector("a");
+if (anchor2) {
     console.log(anchor2.href);
 }
-// Option - 2
-var anchor_2 = document.querySelector("a"); // <-- "!" means I am sure it exist in the DOM
-// Option - 3
-var anchor_3 = document.querySelector("a");
-console.log(anchor_3 === null || anchor_3 === void 0 ? void 0 : anchor_3.href); // <-- "?" means "if it exists, use its href property"
+//------------------
+// Solution - 2
+var anchor_2 = document.querySelector("a");
+console.log(anchor_2 === null || anchor_2 === void 0 ? void 0 : anchor_2.href); // <-- "?" means "if it exists, use its href property"
+//------------------
+// Solution - 3
+var anchor_3 = document.querySelector("a"); // <-- "!" means I am sure it exist in the DOM
+console.log(anchor_3.href);
+//------------------
+// Solution - 4
+var anchor_4 = document.querySelector("a"); // define as HTMLAnchorElement
+console.log(anchor_4.href);
 //------------------------------------------------------------------
-// Forms
-var form = document.querySelector("form"); // TypeScript know the 'a' element
-// Hover on 'form2' indicates 'Element | Null'. 
+//
+//  Handle Forms
+//---
+var form = document.querySelector("form");
+// console.log(form.children); // X Incorrect
+// Hover on 'form1' indicates 'Element | Null'.
 // TypeScript does not know any specific class name
+//---
+// Solution -> use "!" to force explain TypeScript that the element does exist
 var form2 = document.querySelector(".new-item-form");
-// console.log(form2.children);  // X Incorrect
-// Solution = type casting to precise an HTMl form element
+console.log(form2.children); // Correct
+//---
+// Solution -> type casting to precise an HTMl form element
 var form3 = document.querySelector(".new-item-form");
 console.log("form3.children = ", form3.children);
 //------------------------------------------------------------------
 // Input
+//---
 var type = document.querySelector("#type");
+type.addEventListener("change", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
+//---
 var tofrom = document.querySelector("#tofrom");
+tofrom.addEventListener("change", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
+//---
 var details = document.querySelector("#details");
+details.addEventListener("change", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
+//---
 var amount = document.querySelector("#amount");
+amount.addEventListener("focusout", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
 //------------------------------------------------------------------
 // add an event listener
 form3.addEventListener("submit", function (e) {
--- 11_dom_type_casting/src/app.ts
+++ 11_dom_type_casting/src/app.ts
@@ -1,47 +1,63 @@
 /* ---------------------------------
-Typscript DOM & Type Casting*/
+TypeScript DOM & Type Casting*/
 
 //----------------------------------------------------------------
 const anchor = document.querySelector("a");
 
 console.log("anchor = ", anchor);
-//console.log(anchor.href) // Sure that anchor exists to be able to access its "href" prop ?? 
+// console.log(anchor.href); // Error
 
-/* TypeScript didn't parse the DOM, it cannot be sure 
+/* 
+Error const anchor: HTMLAnchorElement | null . 
+'anchor' is possibly 'null'.ts(18047)
+
+TypeScript didn't parse the DOM, it cannot be sure 
 that "anchor" element exists in the DOM */
 
-// Option - 1
+//------------------
+// Solution - 1
 
-if (anchor) { // option-1
-  // do something
-} else {
-  const anchor2 = document.querySelector("a")!; // exclamation mark
+const anchor2 = document.querySelector("a");
+if (anchor2) {
   console.log(anchor2.href);
 }
+//------------------
+// Solution - 2
 
+const anchor_2 = document.querySelector("a");
+console.log(anchor_2?.href); // <-- "?" means "if it exists, use its href property"
 
-// Option - 2
+//------------------
+// Solution - 3
+const anchor_3 = document.querySelector("a")!; // <-- "!" means I am sure it exist in the DOM
+console.log(anchor_3.href);
 
-const anchor_2 = document.querySelector("a")!; // <-- "!" means I am sure it exist in the DOM
-
-// Option - 3
-
-const anchor_3 = document.querySelector("a");
-console.log(anchor_3?.href)  // <-- "?" means "if it exists, use its href property"
+//------------------
+// Solution - 4
+const anchor_4 = document.querySelector("a") as HTMLAnchorElement; // define as HTMLAnchorElement
+console.log(anchor_4.href);
 
 //------------------------------------------------------------------
-// Forms
+//
+//  Handle Forms
 
-const form = document.querySelector("form")!;  // TypeScript know the 'a' element
+//---
 
+const form = document.querySelector("form");
+// console.log(form.children); // X Incorrect
 
-// Hover on 'form2' indicates 'Element | Null'. 
+// Hover on 'form1' indicates 'Element | Null'.
 // TypeScript does not know any specific class name
-const form2 = document.querySelector(".new-item-form");
 
-// console.log(form2.children);  // X Incorrect
+//---
 
-// Solution = type casting to precise an HTMl form element
+// Solution -> use "!" to force explain TypeScript that the element does exist
+const form2 = document.querySelector(".new-item-form")!;
+console.log(form2.children); // Correct
+
+//---
+
+// Solution -> type casting to precise an HTMl form element
 const form3 = document.querySelector(".new-item-form") as HTMLFormElement;
 
 console.log("form3.children = ", form3.children);
@@ -49,18 +65,37 @@ console.log("form3.children = ", form3.children);
 //------------------------------------------------------------------
 // Input
 
+//---
 const type = document.querySelector("#type") as HTMLSelectElement;
+type.addEventListener("change", (event) => {
+  const eventTarget = event.target as HTMLSelectElement;
+  console.log(eventTarget.value);
+});
 
+//---
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+tofrom.addEventListener("change", (event) => {
+  const eventTarget = event.target as HTMLInputElement;
+  console.log(eventTarget.value);
+});
 
+//---
 const details = document.querySelector("#details") as HTMLInputElement;
+details.addEventListener("change", (event) => {
+  const eventTarget = event.target as HTMLInputElement;
+  console.log(eventTarget.value);
+});
 
+//---
 const amount = document.querySelector("#amount") as HTMLInputElement;
-
+amount.addEventListener("focusout", (event) => {
+  const eventTarget = event.target as HTMLInputElement;
+  console.log(eventTarget.value);
+});
 //------------------------------------------------------------------
 // add an event listener
 
-form3.addEventListener("submit", (e: Event) => {
+form3.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
